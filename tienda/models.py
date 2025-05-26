1eb91e1f028fff614429337c214c450686028281
mawernerg <ma.werner@duocuc.cl>
2025-05-26 11:09:09 -0400

    Inicio de modelo, vista y configs varias

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    codprod = models.CharField(max_length = 10)
+    nomprod = models.CharField(max_length = 100)
+    precio = models.IntegerField(null = False)
+    descripcion = models.TextField(max_length = 500)
+    publicado = models.BooleanField(default = False)
+    categoria = models.TextField(max_length = 200, default = 'Sin Categoría')
+
+    def __str__(self):
+        return self.nomprod
+    def __unicode__(self):
+        return self.nomprod
